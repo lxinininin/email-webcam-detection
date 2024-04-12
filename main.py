e86cbc6fb386f7dad00a22f055fb6261dbea723b
lxinininin <lxinin1999@gmail.com>
2024-04-12 16:28:08 +0800

    Program the Webcam to detect moving objects

--- main.py
+++ main.py
@@ -5,8 +5,43 @@ import time
 video = cv2.VideoCapture(0)  # 0 means using the main camera
 time.sleep(1)
 
+first_frame = None
+
 while True:
     check, frame = video.read()
+    # convert frame to gray frame
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # gray_frame_gau will be the grayscale image gray_frame
+    # after applying the Gaussian blur with the specified kernel size.
+    # This operation is often used to reduce noise and detail in images.
+    gray_frame_gau = cv2.GaussianBlur(gray_frame, (21, 21), 0)
+
+    if first_frame is None:
+        first_frame = gray_frame_gau
+
+    # compare the difference between two frames
+    delta_frame = cv2.absdiff(first_frame, gray_frame_gau)
+
+    # 60 is the threshold value.
+    # Pixels with intensity values greater than or equal to 60 will be set to the maximum value (255 in this case),
+    # while pixels with intensity values below 60 will be set to 0.
+    thresh_frame = cv2.threshold(delta_frame, 60, 255, cv2.THRESH_BINARY)[1]
+    dil_frame = cv2.dilate(thresh_frame, None, iterations=2)
+
+    # detect contours
+    contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in contours:
+        # if it is a small object, which means it is perhaps a fake object (maybe is a light difference and so on)
+        if cv2.contourArea(contour) < 5000:
+            continue
+        x, y, w, h = cv2.boundingRect(contour)
+        # this will draw the rectangle around the original frame
+        # (x, y) is the coordinate of left-up corner, (x+w, y+h) is the coordinate of right-down corner
+        # (o, 255, 0) is the color of the rectangle, which is pure green
+        # 3 is the width
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+
     cv2.imshow("My video", frame)
 
     # this creates key object basically
